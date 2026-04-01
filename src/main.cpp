a9c9db3d798c82e6399daec024b152d61de73c63
snoopies <cngz.gva@gmail.com>
2026-04-01 23:26:03 +0800

    Add LED blink code

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+// LED引脚定义
+// ESP32-S3 DevKitC-1 通常使用 GPIO 2 或 GPIO 15 作为内置LED
+#define LED_PIN 2
+
+void setup() {
+  // 初始化串口
+  Serial.begin(115200);
+  
+  // 设置LED引脚为输出
+  pinMode(LED_PIN, OUTPUT);
+  
+  Serial.println("ESP32-S3 LED Blink Started!");
+  Serial.println("LED Pin: " + String(LED_PIN));
+}
+
+void loop() {
+  // 点亮LED
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("LED ON");
+  delay(1000);  // 等待1秒
+  
+  // 熄灭LED
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("LED OFF");
+  delay(1000);  // 等待1秒
+}
